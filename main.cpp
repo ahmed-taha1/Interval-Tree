327f0b4659d6e59b2a93b995e34422cf213e8847
Youssef Moataz <yousefcrunshy03@gmail.com>
2023-12-19 19:31:03 +0200

    .

--- main.cpp
+++ main.cpp
@@ -17,111 +17,133 @@ struct Interval{
     }
 };
 
+ostream& operator <<(ostream & out, Interval interval){
+    if (interval.start == INT_MAX && interval.end == INT_MAX)
+    {
+        out << "Interval doesn't overlap with any intervals" << '\n';
+    }
+    else
+    {
+        out << interval.start << " " << interval.end << '\n';
+    }
+    return out;
+}
+
 struct Node{
     Interval interval;
-    int mex;
+    int maxHighInSubtree;
     Node* left;
     Node* right;
     Node(){
-        mex = -1;
+        maxHighInSubtree = -1;
         left = nullptr;
         right = nullptr;
     }
 };
 
 class IntervalTree{
-    private:
-        Node* root;
-        int insertInterval(Node *curr, Interval interval)
+private:
+    Node* root;
+    int insert(Node *curr, Interval interval)
+    {
+        int insertedMex = 0;
+        // the current interval is less than to the interval that we want to InsertInterval so, go to the curr-> right
+        if( (interval.start > curr->interval.start) || (interval.start == curr->interval.start && interval.end > curr->interval.end))
         {
-            int insertedMex = 0;
-            // the current interval is less than to the interval that we want to insert so, go to the curr-> right
-            if( (interval.start > curr->interval.start) || (interval.start == curr->interval.start && interval.end > curr->interval.end))
+            if(curr->right == nullptr)
             {
-                if(curr->right == nullptr)
-                {
-                    Node* node = new Node();
-                    node->interval = interval;
-                    node->mex = interval.end;
-                    curr->right = node;
-                    insertedMex = node->mex;
-                }
-                else insertedMex = insertInterval(curr->right, interval);
+                Node* node = new Node();
+                node->interval = interval;
+                node->maxHighInSubtree = interval.end;
+                curr->right = node;
+                insertedMex = node->maxHighInSubtree;
             }
-            // the current interval is greater than to the interval that we want to insert so, go to the curr-> left
-            else if( (interval.start < curr->interval.start) || (interval.start == curr->interval.start && interval.end < curr->interval.end))
-            {
-                if(curr->left == nullptr)
-                {
-                    Node* node = new Node();
-                    node->interval = interval;
-                    node->mex = interval.end;
-                    curr->left = node;
-                    insertedMex = node->mex;
-                }
-                else insertedMex = insertInterval(curr->left, interval);
-            }
-
-            if(insertedMex > curr->mex)
-            {
-                curr->mex = insertedMex;
-            }
-            return insertedMex;
+            else insertedMex = insert(curr->right, interval);
         }
-
-    public:
-        IntervalTree()
+        // the current interval is greater than to the interval that we want to InsertInterval so, go to the curr-> left
+        else if( (interval.start < curr->interval.start) || (interval.start == curr->interval.start && interval.end < curr->interval.end))
         {
-            root = nullptr;
-        }
-
-        void insert(Interval interval)
-        {
-            if(root == nullptr)
+            if(curr->left == nullptr)
             {
-                root = new Node();
-                root->interval = interval;
-                root->mex = interval.end;
+                Node* node = new Node();
+                node->interval = interval;
+                node->maxHighInSubtree = interval.end;
+                curr->left = node;
+                insertedMex = node->maxHighInSubtree;
             }
-            else insertInterval(root, interval);
+            else insertedMex = insert(curr->left, interval);
         }
 
-        void bfs(){
-            queue<Node*> q;
-            q.push(root);
-            while (!q.empty())
+        if(insertedMex > curr->maxHighInSubtree)
+        {
+            curr->maxHighInSubtree = insertedMex;
+        }
+        else
+        {
+            insertedMex = curr->maxHighInSubtree;
+        }
+        return insertedMex;
+    }
+
+public:
+    IntervalTree()
+    {
+        root = nullptr;
+    }
+
+    void InsertInterval(Interval interval)
+    {
+        if(root == nullptr)
+        {
+            root = new Node();
+            root->interval = interval;
+            root->maxHighInSubtree = interval.end;
+        }
+        else insert(root, interval);
+    }
+
+    Interval SearchInterval(Interval query)
+    {
+        Node* curr = root;
+        Interval minInterval(INT_MAX, INT_MAX);
+        while (curr != nullptr)
+        {
+            // store the current interval if it overlaps the query interval and it is less than the low in the current minInterval
+            if(query.start <= curr->interval.end && curr->interval.start <= query.end && minInterval.start > curr->interval.start){
+                minInterval = curr->interval;
+            }
+
+            if(curr->left != nullptr && query.start <= curr->left->maxHighInSubtree)
             {
-                Node* curr = q.front();
-                q.pop();
-                cout << curr->interval.start << " " << curr->interval.end << '\n';
-
-                if(curr->right != nullptr)
-                    q.push(curr->right);
-
-                if(curr->left != nullptr)
-                    q.push(curr->left);
+                curr = curr->left;
             }
+            else curr = curr->right;
         }
+        return minInterval;
+    }
 
-        void printDfs(Node* curr)
-        {
-            if(curr == nullptr)
-                return;
-            cout << curr->interval.start << " " << curr->interval.end << '\n';
-            printDfs(curr->left);
-            printDfs(curr->right);
-        }
-        void dfs()
-        {
-            printDfs(root);
-        }
+    void printDfs(Node* curr)
+    {
+        if(curr == nullptr)
+            return;
+        cout << curr->interval.start << " " << curr->interval.end << '\n';
+        printDfs(curr->left);
+        printDfs(curr->right);
+    }
+    void dfs()
+    {
+        printDfs(root);
+    }
 };
 
-int main() {
+int main()
+{
     IntervalTree intervalTree;
-    intervalTree.insert(Interval(1,2));
-    intervalTree.insert(Interval(2,3));
-    intervalTree.insert(Interval(0,21));
-    intervalTree.insert(Interval(3,21));
+    intervalTree.InsertInterval(Interval(1, 2));
+    intervalTree.InsertInterval(Interval(2, 3));
+    intervalTree.InsertInterval(Interval(0, 21));
+    intervalTree.InsertInterval(Interval(3, 21));
+    intervalTree.InsertInterval(Interval(40, 100));
     intervalTree.dfs();
+    cout << intervalTree.SearchInterval(Interval(101, 200));
 }
